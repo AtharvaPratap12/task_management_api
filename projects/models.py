074b3e4619f114949a07822ec5f684cd65607633
Atharva Pratap <atharvapratap0610@gmail.com>
2026-04-15 17:20:26 +0530

    Added project and task models with migrations

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length = 200)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'projects')
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
+
+class ProjectMember(models.Model):
+    project = models.ForeignKey(Project, on_delete = models.CASCADE, related_name = 'members')
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'project_members')
+    role = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.project.name} ({self.role})"
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projects.models import Project
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ("Todo", "Todo"),
+        ("In Progress", "In Progress"),
+        ("Done", "Done")
+    ]
+
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High')
+    ]
+
+    title = models.CharField(max_length = 200) 
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete = models.CASCADE, related_name = 'tasks')
+    assigned_to = models.ForeignKey(User, on_delete = models.CASCADE)
+    status = models.CharField(max_length = 20, choices = STATUS_CHOICES, default = "Todo")
+    priority = models.CharField(max_length = 20, choices = PRIORITY_CHOICES, default = "Medium")
+    due_date = models.DateField()
